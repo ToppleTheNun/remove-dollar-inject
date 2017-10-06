50aa5a06fe581b2478187c50c491be40fefbddb5
Richard Harrah <topplethenunnery@gmail.com>
2017-10-06 01:41:29 -0400

    ci(.travis/after-script.sh): changes if to use [[]] instead of []

--- .travis/after-script.sh
+++ .travis/after-script.sh
@@ -1,6 +1,6 @@
 # after-script.sh
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_NODE_VERSION" == "8" ]
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_NODE_VERSION" == "8" ]]
 then
     ./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
 fi
