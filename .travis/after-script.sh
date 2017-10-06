7d1cb91571f9569eaa65aab2d012435f8a9df9ae
Richard Harrah <topplethenunnery@gmail.com>
2017-10-06 01:38:48 -0400

    ci(.travis/after-script.sh): adds CodeClimate reporting

--- .travis/after-script.sh
+++ .travis/after-script.sh
@@ -1,8 +1,6 @@
 # after-script.sh
 #!/bin/bash
-echo "TRAVIS_NODE_VERSION=$TRAVIS_NODE_VERSION"
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_NODE_VERSION" == "8" ]
 then
-    echo "We're not in a pull request!"
-    # ./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
+    ./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
 fi
