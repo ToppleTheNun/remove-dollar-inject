43e173c336d92c9f3c23a7a7a934350933707cf2
Harrah, Richard <topplethenunnery@gmail.com>
2019-01-28 11:16:44 -0500

    ci(coverage): switch to codecov

--- .travis/after-script.sh
+++ .travis/after-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_NODE_VERSION" == "8" ]]
 then
-    ./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
+    yarn codecov
 fi
