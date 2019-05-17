b119505f37345e9f69d0209ed2f853a0fa7ecc18
Richard Harrah <Nunnery@users.noreply.github.com>
2019-05-17 09:04:22 -0400

    ci(travis): run semantic-release on latest node

--- .travis/after-success.sh
+++ .travis/after-success.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_NODE_VERSION" == "8" ]]
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_NODE_VERSION" == "12" ]]
 then
     npm run semantic-release
 fi
